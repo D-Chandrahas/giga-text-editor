9b3a053c6b3980a0dc9d4c5371dd921b20f4e790
d-chandrahas <chandrahasreddy732@gmail.com>
2022-10-09 18:00:44 +0530

    bug fix

--- src/giga.cpp
+++ src/giga.cpp
@@ -1,12 +1,13 @@
 #include "main_header.hpp"
 
 
+int resize_flag = 0;
+
 void resize_handler(int sig){
 	resize_flag = 1;
 }
 
 int main(int argc, char** argv){
-	resize_flag = 0;
 	signal(SIGWINCH, resize_handler);
 	std::string filepath = "";
 	if(argc > 1){
